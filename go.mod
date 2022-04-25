f8aaf8d946d71fd9c422c69f285c965929cae474
bashery <ahmedalbachiry@gmail.com>
2022-04-25 14:33:38 +0300

    megrate from fiber to echo lib

--- go.mod
+++ go.mod
@@ -1,8 +1,25 @@
-module templ
+module medium
 
-go 1.14
+go 1.18
 
 require (
-	github.com/gofiber/fiber v1.14.1 // indirect
-	github.com/gofiber/template v1.5.5 // indirect
+	github.com/go-sql-driver/mysql v1.6.0
+	github.com/labstack/echo/v4 v4.7.2
+)
+
+require (
+	github.com/CloudyKit/fastprinter v0.0.0-20200109182630-33d98a066a53 // indirect
+	github.com/CloudyKit/jet v2.1.2+incompatible // indirect
+	github.com/dannyvankooten/echojet v0.0.0-20180721144521-283bee0cb051 // indirect
+	github.com/labstack/echo v3.3.10+incompatible // indirect
+	github.com/labstack/gommon v0.3.1 // indirect
+	github.com/mattn/go-colorable v0.1.11 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/stretchr/testify v1.7.1 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.1 // indirect
+	golang.org/x/crypto v0.0.0-20220411220226-7b82a4e95df4 // indirect
+	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
+	golang.org/x/sys v0.0.0-20211103235746-7861aae1554b // indirect
+	golang.org/x/text v0.3.7 // indirect
 )
--- main.go
+++ main.go
@@ -1,30 +1,103 @@
 package main
 
 import (
-	"github.com/gofiber/fiber"
-	"github.com/gofiber/template/html"
+	"database/sql"
+	"io"
+	"text/template"
+
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/labstack/echo/v4"
 )
 
-type Dog struct {
-	Name string
-	Age  int
+type User struct {
+	Id     int
+	Name   string
+	Email  string
+	Phon   string
+	Avatar string
 }
 
 func main() {
-	fmt.Printn("medium project")
-
-	engine := html.New("templates", ".html")
-
-	app := fiber.New(&fiber.Settings{Views: engine})
-
-	app.Get("/", dogFunc)
-
-	app.Listen(":8000")
+	db := setdb()
+	defer db.Close()
+	e := echo.New()
+	e.Renderer = t
+	e.GET("/hello", humenFunc)
+	fmt.Println(e.Start(":8080"))
 }
 
-func dogFunc(c *fiber.Ctx) {
-	dog := &Dog{"boby", 3}
-	_ = c.Render("index", fiber.Map{
-		"Name": dog.Name, "Age": dog.Age,
-	})
+func humenFunc(c echo.Context) (err error) {
+	user := getUserInfo(1)
+	fmt.Println(user)
+
+	err = c.Render(200, "index.html", user) // map[string]User{"User": user})
+	if err != nil {
+		fmt.Println(err)
+	}
+	return err
+}
+
+// gets all user information for update this info
+func getUserInfo(userid int) (user User) {
+	err := db.QueryRow(
+		"SELECT username, email,phon, linkavatar FROM stores.users WHERE userid = ?",
+		userid).Scan(&user.Name, &user.Email, &user.Phon, &user.Avatar)
+	if err != nil {
+		fmt.Println("no result or", err.Error())
+	}
+	return user
+}
+
+var (
+	db  *sql.DB
+	err error
+)
+
+type Template struct {
+	templates *template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+var t = &Template{
+	templates: template.Must(template.ParseGlob("templates/*.html")),
+}
+
+func setdb() *sql.DB {
+	db, err = sql.Open("mysql", "root:123456@tcp(127.0.0.1:3306)/?charset=utf8&parseTime=True&loc=Local")
+	if err != nil { // why no error when db is not runinig ??
+		fmt.Println("error when open mysql server", err)
+		// TODO report this error.
+		os.Exit(1)
+
+	}
+
+	if err = db.Ping(); err != nil {
+
+		fmt.Println("error when ping to database", err)
+		switch {
+		case strings.Contains(err.Error(), "connection refused"):
+			// TODO handle errors by code of error not by strings.
+
+			cmd := exec.Command("mysql.server", "restart")
+			// for systemd linux : exec.Command("sudo", "service", "mariadb", "start")
+			//cmd.Stdin = strings.NewReader(os.Getenv("JAWAD"))
+			errc := cmd.Run()
+			if errc != nil {
+				fmt.Println("error when run database cmd ", errc)
+			}
+		default:
+			fmt.Println("error at  setdb() func, db.Ping() func")
+			fmt.Println("unknown this error", err)
+			os.Exit(1)
+		}
+	}
+	return db
 }
